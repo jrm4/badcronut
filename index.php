c2c863fc5060d90486307be271c0f1611589c84a
John Marks <git@jrm4.com>
2021-06-29 20:47:54 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Code Input Form - Best</title>
+</head>
+
+<body>
+
+<h4> Please enter your information</h4>
+
+	<form action="output.php" method="post">
+	
+            Please enter your first name: <input name="first" type="text" />
+            <br>
+            Please enter your last name: <input name="last" type="text" />
+            <br>
+            What is your age? <input name="age" type="text" />
+            <br>
+            What is your income? <input name="income" type="text" />
+            <br>
+        
+            <input type="submit" />
+       </form>
+
+<br>
+<br>
+
+</body>
+</html>
