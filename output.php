1873b3bac11cb76d3c2570770f21eaed93a6ac6a
John Marks <git@jrm4.com>
2021-06-29 21:18:12 -0400

    Here goes nothin

--- output.php
+++ output.php
@@ -1,8 +1,19 @@
 <?php
 
+include './classes.php';
+
 	$first = $_POST['first'];
 	$last = $_POST['last'];	
 	$age = $_POST['age'];
 	$income = $_POST['income'];
 	
+$currsub = new subject(); 
+
+$currsub->setFirstname($first);
+$currsub->setLastname($last);
+$currsub->setAge($age);
+$currsub->setIncome($income);
+	
+
+
 ?>
